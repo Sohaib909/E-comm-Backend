d222fdb915ba715db862da029755e6d89ea16704
Sohaib909 <sohaibrafiq991@gmail.com>
2025-09-02 22:18:26 +0500

    Stop tracking node_modules and update .gitignore

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (uri) => {
+  try {
+    const conn = await mongoose.connect(uri);
+    console.log("MongoDB connected:", conn.connection.host);
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import { connectDB } from "./config/db.js";
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+connectDB(MONGODB_URI).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+  );
+});
